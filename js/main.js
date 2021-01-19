87f4e1391ff6c674f06ba81527768424450485ae
Leonardo Thomaz <leonardothomazadv@outlook.com>
2021-01-19 17:24:01 -0300

    criação do projeto

---
+++ js/main.js
@@ -0,0 +1,101 @@
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+    window.open("https://github.com/");
+    //window.location.href = "https://github.com/";
+}
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto");
+}
+
+function voltar(){
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+
+}
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+
+var validar = 0;
+
+function validaIdade(idade){
+    if(idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade?");
+console.log(validaIdade(idade));
+*/
+
+/*
+alert(soma (5, 10));
+
+/*
+var d = new Date();
+alert(d);
+*/
+
+/*
+var count;
+for(count=0; count <=5; count++){
+    alert(count);
+}
+*/
+
+/*
+var count = 5;
+while(count <= 5){
+    console.log(count);
+    count = count +1;
+};
+*/
+
+/*
+var idade = prompt("Qual sua idade?");
+if (idade >= 18 );{
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+/*
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas.nome);
+*/
+
+/*
+var fruta = {nome:"maça", cor "vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+//var nome = "Leonardo Thomaz";
+//var idade = 21;
+//var idade2 = 10;
+//alert(nome + "tem" + idade + "anos");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade + idade2);
